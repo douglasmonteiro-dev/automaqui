ea9f97b993fc1b456a1dbbd11cd386cc63822a04
Douglas Monteiro <douglasmonteiro@icloud.com>
2022-02-25 16:06:31 -0300

    add lecaweb database

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   jwt_secret: "secret=ascacaw21",//process.env.jwt_secret,
-  mongouri: "mongodb://localhost:27017/automaqui"//process.env.mongouri,
+  mongouri: "mongodb://vps14938.publiccloud.com.br:27017/automaqui"//process.env.mongouri,
 };
