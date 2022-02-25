d016429db1fc6400801abcad1131e80bc0e8ac5a
Douglas Monteiro <douglasmonteiro@icloud.com>
2022-02-25 19:15:17 -0300

    update connection db

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   jwt_secret: "secret=ascacaw21",//process.env.jwt_secret,
-  mongouri: "mongodb://admin:automaqui@vps14938.publiccloud.com.br:27017/automaqui"//process.env.mongouri,
+  mongouri: "mongodb://191.252.201.155:27017/automaqui"//process.env.mongouri,
 };
