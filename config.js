203fcc24f1665d883cf3d158ec64e6dc6aa203f5
Douglas Monteiro <douglasmonteiro@icloud.com>
2022-04-16 11:37:37 -0300

    up env

--- config.js
+++ config.js
@@ -1,4 +1,20 @@
+const {config} = require('dotenv');
+const replace = require('@rollup/plugin-replace');
+
+const production = !process.env.ROLLUP_WATCH;
+
 module.exports = {
+  plugins: [
+    replace({
+      // stringify the object       
+      __myapp: JSON.stringify({
+        env: {
+          isProd: production,
+          ...config().parsed // attached the .env config
+        }
+      }),
+    }),
+  ],
   jwt_secret: process.env.jwt_secret,
   mongouri: process.env.mongouri,
   port: process.env.port
