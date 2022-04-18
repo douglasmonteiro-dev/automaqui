95a67437cf98e7f52ab0833648b58297ee00ea59
Douglas Monteiro <douglasmonteiro@icloud.com>
2022-04-18 20:38:33 -0300

    up

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   jwt_secret: '12d3s1ds2ds3s1adadassaasa23',//process.env.jwt_secret,
-  mongouri: 'mongodb://admin:admin@vps14938.publiccloud.com.br:27017', //process.env.mongouri,
+  mongouri: 'mongodb://admin:admin@191.252.201.155:27017', //process.env.mongouri,
   port: 3000//process.env.port
 };
