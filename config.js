eb03a9f099255a101c60757eb20bb7a9c23941b9
Douglas Monteiro <douglasmonteiro@icloud.com>
2022-04-19 08:32:27 -0300

    up env

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   jwt_secret: '12d3s1ds2ds3s1adadassaasa23',//process.env.jwt_secret,
   mongouri: 'mongodb://vitrine:senhavitrine@191.252.201.155:27017', //process.env.mongouri,
-  port: 3000//process.env.port
+  port: process.env.PORT
 };
